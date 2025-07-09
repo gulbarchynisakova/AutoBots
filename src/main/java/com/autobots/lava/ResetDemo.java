8a3e9fd5a4512adbb0710dd95fcf1b1feaafdff7
Gulbarchyn <gulbarchynisakova120@jmail.com>
2025-07-09 22:17:46 +0600

    version 1.0.1

--- src/main/java/com/autobots/lava/ResetDemo.java
+++ src/main/java/com/autobots/lava/ResetDemo.java
@@ -3,5 +3,7 @@ package com.autobots.lava;
 public class ResetDemo {
     public static void main(String[] args) {
         System.out.println("version 1.0.0");
+        System.out.println("version 1.0.1");
+
     }
 }
